0110ddaf343cfaadbb26cb05bb26b114514bdb80
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-05-29 15:35:49 -0500

    Bonus done

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -2,13 +2,18 @@ console.log("time to start the timer!");
 
 
 const start = document.getElementById('start');
+let date = new Date();
 
 
+function timer(){ setInterval(function(){
+    let time = 0;
+    
+    if(time>=0){
+        let display = document.getElementById('display');
+        display.innerHTML = time;
+        console.log(time);
+        time +=1;
+    }
+    
 
-function timer(){
-    let second = 0;
-    second += 1;
-    let display = document.getElementById('display');
-    display.innerHTML = second++;
-
-}
+},1000);}
